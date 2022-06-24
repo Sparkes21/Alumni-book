c43755ef161acf7d4c3879f6500e3f23795fa07e
Josh Sparkes <jshsparx@gmail.com>
2022-06-24 15:16:31 -0600

    calculating sales and tax for each company but for loop stops after first telus and does not count the second

--- sales-tax.js
+++ sales-tax.js
@@ -8,36 +8,39 @@ const companySalesData = [
   {
     name: "Telus",
     province: "BC",
-    sales: [ 100, 200, 400 ]
+    sales: [100, 200, 400]
   },
   {
     name: "Bombardier",
     province: "AB",
-    sales: [ 80, 20, 10, 100, 90, 500 ]
+    sales: [80, 20, 10, 100, 90, 500]
   },
   {
     name: "Telus",
     province: "SK",
-    sales: [ 500, 100 ]
+    sales: [500, 100]
   }
 ];
 
-
 const calculateSalesTax = (salesData, taxRates) => {
   const results = {};
+  // loop through the companies in the list
   for (let i = 0; i < salesData.length; i++) {
-    for (let j = 0; j< salesData[i]['sales'].length; j++) {
-      results[salesData[i]['name']].totalSales += salesData[i].sales[j];
-      results[salesData[i]['name']].totalTaxes += salesData[i].sales[j] * taxRates[salesData[i].province];
-    }
-  } else {
-    for (let )
-  }
-  // let sum = 0;
-  // for (let i = 0; i < sales.length; i++) {
-  //   sum += array[i]
-  // };
+    // initialize results object if name doesnt exist
+    if (!results[salesData[i]['name']]){
 
-  totalSales = salesData[sales]
-  totalTaxes = totalSales * taxRates
-}
+      results[salesData[i]['name']] = { totalSales: 0, totalTaxes: 0 };
+      
+      for (let j = 0; j < salesData[i]['sales'].length; j++) {
+        results[salesData[i]['name']].totalSales += salesData[i].sales[j];
+        results[salesData[i]['name']].totalTaxes += salesData[i].sales[j] * taxRates[salesData[i].province];
+      } 
+    }
+    }
+    
+    console.log(results);
+    return results;
+  }
+
+  calculateSalesTax(companySalesData, salesTaxRates)
+
