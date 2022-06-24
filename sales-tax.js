602c4850dd97c5be7116d70346cd38c2acda79e2
Josh Sparkes <jshsparx@gmail.com>
2022-06-24 15:06:30 -0600

    nested for loops to loop through companies and sales data within those companies

--- sales-tax.js
+++ sales-tax.js
@@ -1,3 +1,43 @@
-const calculateSalesTax = (salesData, taxRoutes) => {
+const salesTaxRates = {
+  AB: 0.05,
+  BC: 0.12,
+  SK: 0.10
+};
 
+const companySalesData = [
+  {
+    name: "Telus",
+    province: "BC",
+    sales: [ 100, 200, 400 ]
+  },
+  {
+    name: "Bombardier",
+    province: "AB",
+    sales: [ 80, 20, 10, 100, 90, 500 ]
+  },
+  {
+    name: "Telus",
+    province: "SK",
+    sales: [ 500, 100 ]
+  }
+];
+
+
+const calculateSalesTax = (salesData, taxRates) => {
+  const results = {};
+  for (let i = 0; i < salesData.length; i++) {
+    for (let j = 0; j< salesData[i]['sales'].length; j++) {
+      results[salesData[i]['name']].totalSales += salesData[i].sales[j];
+      results[salesData[i]['name']].totalTaxes += salesData[i].sales[j] * taxRates[salesData[i].province];
+    }
+  } else {
+    for (let )
+  }
+  // let sum = 0;
+  // for (let i = 0; i < sales.length; i++) {
+  //   sum += array[i]
+  // };
+
+  totalSales = salesData[sales]
+  totalTaxes = totalSales * taxRates
 }
