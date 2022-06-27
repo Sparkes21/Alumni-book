244bfebc13b8f3d389ef345add457f6c1ce195b2
Josh Sparkes <jshsparx@gmail.com>
2022-06-27 16:02:34 -0600

    create stock market file and implement function, returning wrong answer currently

---
+++ Stock-market.js
@@ -0,0 +1,19 @@
+const maxProfit = (array) => {
+  let maxProfit = -1;
+  let buyPrice = 0;
+  let sellPrice = 0;
+
+
+  for (let i = 0; i <= array.length; i++) {
+    buyPrice = array[i];
+    sellPrice = array[i + 1];
+    if (sellPrice > buyPrice) {
+      maxProfit = sellPrice - buyPrice
+    }
+  }
+  console.log(maxProfit);
+  return maxProfit;
+}
+
+maxProfit([45, 24, 35, 31, 40, 38, 11]);
+
